b42fee0667fad8fd2c816db53be862cded065d30
AgelxNash <iam@agel-nash.ru>
2013-09-27 18:16:54 +0600

    UltimateParent

--- core/components/getobjfield/model/modResourceCustomData.class.php
+++ core/components/getobjfield/model/modResourceCustomData.class.php
@@ -2,13 +2,30 @@
 include_once(dirname(__FILE__)."/modResourceData.class.php");
 
 class modResourceCustomData extends modResourceData{
+	
+	protected static $localConfigDefault = array(
+		'top' => 0
+	);
+	
+	public function __construct(\modX &$modx, array $config = array()) {
+		parent::__construct($modx, $config);
+		parent::$ConfigDefault = array_merge(parent::$ConfigDefault, self::$localConfigDefault);
+	}
+	
 	public function checkConfig($config){
 		parent::checkConfig($config);
 		$this->_config['object'] = 'modResource';
+		$this->_config['top'] = $this->getOption('top', $config);
 	}
 	
 	public function getData($id, $field){
 		$value = null;
+		if($this->getOption('top')>0){
+			$top = $this->getOption('top');
+			for($i=0; $i<$top; $i++){
+				$id = \getObjField\defaultData::getData($id, 'parent');
+			}
+		}
 		switch($field){
 			case 'title':{
 				$value = parent::getData($id, 'longtitle');
